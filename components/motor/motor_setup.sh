a26f6d5fb6543fb90c0910e7b8aa1bfe1d57d9db
Minh-An Dao <minhan7497@gmail.com>
2020-04-15 17:33:37 +0700

    change dir

---
+++ components/motor/motor_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu -o pipefail # fail on error , debug all lines
+
+if [ 'root' != $( whoami ) ] ; then
+  echo "Please run as root! ( sudo ${0} )"
+  exit 1;
+fi
+
+# Pyserial package install  ## https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
+apt-get install python3-serial -y
+if [ 0 -eq $( grep -c 'enable_uart = 1' /boot/config.txt ) ]; then # check if the phrase "enable_uart = 1" existed in the file or not
+	echo "enable_uart = 1" | tee -a /boot/config.txt
+fi
+
+
+
+## (c) 2020 Minh-An Dao. All right reserved
