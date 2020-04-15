aa11e81655e107070b746f99f435f10d4e41615e
Minh-An Dao <minhan7497@gmail.com>
2020-04-15 23:43:56 +0700

    try new motor function

--- components/motor/main.py
+++ components/motor/main.py
@@ -1,12 +1,26 @@
 """------------------------------------------------------------*-
-  Main process for MIS-CTU UV robot
+  Example of motor controlling for MIS-CTU UV robot
   Tested on: Raspberry Pi 3 B+
   (c) Minh-An Dao 2020
-  version 1.00 - 12/04/2020
+  version 1.00 - 15/04/2020
  --------------------------------------------------------------
  *
  *
  --------------------------------------------------------------"""
-import server
+from motor import Motor
 
-server.run()
+try:
+  while True:
+    data = input('What do you want to do?f/b/l/r/s')
+    if data == 'f':
+      Motor.move_fw(50)
+    if data == 'b':
+      Motor.move_bw(50)
+    if data == 'l':
+      Motor.turn_left(50)
+    if data == 'r':
+      Motor.turn_right(50)
+    if data == 's':
+      Motor.release()
+except KeyboardInterrupt:
+  pass
