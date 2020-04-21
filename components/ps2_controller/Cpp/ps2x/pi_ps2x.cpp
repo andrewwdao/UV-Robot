be26edfa467b4387fd4a28b35df7cd39393c74e8
Minh-An Dao <minhan7497@gmail.com>
2020-04-21 17:16:36 +0700

    try 0xFF

--- components/ps2_controller/Cpp/ps2x/pi_ps2x.cpp
+++ components/ps2_controller/Cpp/ps2x/pi_ps2x.cpp
@@ -221,7 +221,7 @@ PS2X::~PS2X() {}//end destructor
 void PS2X::changeMode(bool analog_mode, bool locked_mode)
 {
     set_mode[3] = (analog_mode)?0x01:0x00; //change between analog and digital mode
-    set_mode[4] = (locked_mode)?0x03:0x01; //change between lock and unlock mode (press the MODE button on the controller to change between analog and digital)
+    set_mode[4] = (locked_mode)?0x03:0xFF; //change between lock and unlock mode (press the MODE button on the controller to change between analog and digital)
     printf("0x%02X\n",set_mode[4]);
 }//end changeMode
 
