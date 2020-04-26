6024d2d200bed5716ceb5687134c32487c37287f
Minh-An Dao <minhan7497@gmail.com>
2020-04-26 14:29:34 +0700

    fix typos

--- main/motor/testing_motor.py
+++ main/motor/testing_motor.py
@@ -69,15 +69,15 @@ RAD_STEP = pi/2*DUTY_CYCLE/ACCEL_TIME
 time.sleep(1)
 print('running...')
 for x in range(0, 100000):
-    pos += 0.01
+    pos += 0.005
     if rad < pi/2:
         rad += RAD_STEP
     speed = MAX_SPEED*sin(rad)
-    cmd = "{N0 P" + str(round(pos,2)) + " V" + str(round(speed, 2)) + "}" # {N1 P500 V100} - set position and speed for PID
+    cmd = "{N0 P" + str(round(pos,3)) + " V" + str(round(speed, 3)) + "}" # {N1 P500 V100} - set position and speed for PID
     cmd = cmd.encode('utf-8')
     __serial.write(cmd) # send to the driver
     time.sleep(DUTY_CYCLE)  # sleep for 4us --> 250kHz
-    print(str(round(speed, 2)))
+    print(str(round(speed, 3)))
 
 # ACCEL_TIME = 3
 # DUTY_CYCLE = 0.001 # 1kHz
