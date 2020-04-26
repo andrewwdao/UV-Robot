ab73bdad21df3ce9a5f30bae289d529b824fc96e
Minh-An Dao <minhan7497@gmail.com>
2020-04-26 10:28:52 +0700

    debugging

--- main/motor/testing_motor.py
+++ main/motor/testing_motor.py
@@ -48,7 +48,7 @@ starter_cmd = "{N0 M2 A2000 R}" # Set all motors to PID mode, with Acceleration 
 starter_cmd = starter_cmd.encode('utf-8')
 __serial.write(starter_cmd)
 pos = 0
-speed = 50
+speed = 00
 # pos_2 = 0
 # speed_2 = 0
 time.sleep(1)
@@ -65,8 +65,8 @@ for x in range(0, 3000):
     pos += 1
     if speed < 300:
         speed += 0.1
-    cmd = "{N0 P" + str(pos) + " V" + str(speed) + "}" # {N1 P500 V100} - set position and speed for PID
+    cmd = "{N0 P" + str(pos) + " V" + str(round(speed, 1)) + "}" # {N1 P500 V100} - set position and speed for PID
     cmd = cmd.encode('utf-8')
     __serial.write(cmd) # send to the driver
     time.sleep(0.001)  # sleep for 4us --> 250kHz
-    print(str(speed))
+    print(str(round(speed, 1)))
