8f22aa2ab744056db61387279fc3ecb613d4b926
Minh-An Dao <minhan7497@gmail.com>
2020-04-26 10:24:02 +0700

    reduce sleep time

--- main/motor/testing_motor.py
+++ main/motor/testing_motor.py
@@ -64,9 +64,9 @@ print('running...')
 for x in range(0, 3000):
     pos += 1
     if speed < 300:
-        speed += 1
+        speed += 0.1
     cmd = "{N0 P" + str(pos) + " V" + str(speed) + "}" # {N1 P500 V100} - set position and speed for PID
     cmd = cmd.encode('utf-8')
     __serial.write(cmd) # send to the driver
     time.sleep(0.001)  # sleep for 4us --> 250kHz
-    print(pos)
+    print(str(speed))
