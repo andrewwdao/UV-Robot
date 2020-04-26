0aed396bbdeaf1a79e20a23a68efd8caf3b35269
Minh-An Dao <minhan7497@gmail.com>
2020-04-26 10:41:40 +0700

    increase speed

--- main/motor/testing_motor.py
+++ main/motor/testing_motor.py
@@ -64,7 +64,7 @@ print('running...')
 for x in range(0, 3000):
     pos += 1
     if speed < 300:
-        speed += 0.1
+        speed += 0.5
     cmd = "{N0 P" + str(pos) + " V" + str(round(speed, 1)) + "}" # {N1 P500 V100} - set position and speed for PID
     cmd = cmd.encode('utf-8')
     __serial.write(cmd) # send to the driver
