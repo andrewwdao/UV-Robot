1cbdebc7ec513758ace2758b059b695c89dfd59c
Minh-An Dao <minhan7497@gmail.com>
2020-04-26 09:53:09 +0700

    add testing file

--- main/motor/testing_motor.py
+++ main/motor/testing_motor.py
@@ -59,4 +59,4 @@ for x in range(0, 30000):
     cmd = "{N0 P" + str(pos) + " V" + str(speed) + "}" # {N1 P500 V100} - set position and speed for PID
     cmd = cmd.encode('utf-8')
     __serial.write(cmd) # send to the driver
-    time.sleep(0.000004)  # sleep for 4us --> 250kHz
+    time.sleep(0.00001)  # sleep for 4us --> 250kHz
