0747b15b5bb20e24912a3420193762e7338ef703
Minh-An Dao <minhan7497@gmail.com>
2020-05-02 13:55:01 +0700

    clean up motor code

--- main/motor/__init__.py
+++ main/motor/__init__.py
@@ -25,7 +25,7 @@
 
  * In this specific version, PWM mode will be used.
  --------------------------------------------------------------"""
-from motor.motor import MotorUART_PID, MotorUART_PWM
+from motor.motor import MotorUART_PWM#, MotorUART_PID
 
 
 PORT = '/dev/ttyUSB0'
@@ -33,6 +33,3 @@ BAUDRATE = 250000
 MAX_SPEED = 600
 
 Motor = MotorUART_PWM(PORT, BAUDRATE, MAX_SPEED)
-# Motor = MotorUART_PWM(PORT, BAUDRATE)
-
-
