cbce480e3d3de92c38b88632d78741624a99b7b1
Minh-An Dao <minhan7497@gmail.com>
2020-05-30 15:51:20 +0700

    fix typos

--- main/main.py
+++ main/main.py
@@ -273,7 +273,7 @@ def main():  # Main program block
     while True:
         ps2.update()
         sv_cmd = server.read()
-        if sv_cmd:
+        if sv_cmd is not None:
             print(sv_cmd)
 
         cmd_update()
