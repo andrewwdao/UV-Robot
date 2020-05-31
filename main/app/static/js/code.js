9ec174162774f7601c5a78a4aa3c10d77b478fe5
Minh-An Dao <minhan7497@gmail.com>
2020-05-31 10:50:31 +0700

    debugging stupid error

--- main/app/static/js/code.js
+++ main/app/static/js/code.js
@@ -25,7 +25,7 @@
 // })
 
 // ref: https://www.w3schools.com/jsref/dom_obj_event.asp
-// document.addEventListener("keypress", keyPressed, false);
+document.addEventListener("keypress", keyPressed, false);
 document.addEventListener("keydown", keyIsPressing, false);
 document.addEventListener("keyup", keyReleased, false);
 
