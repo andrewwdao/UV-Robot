58cf605f95bbb0e1f06625a3a13ed8127739d84a
Minh-An Dao <minhan7497@gmail.com>
2020-05-31 11:51:12 +0700

    modify how speed changing works

--- main/app/static/js/code.js
+++ main/app/static/js/code.js
@@ -52,6 +52,7 @@ socket.on('connect', () => {
 
 socket.on('speed', () => {
     document.getElementById('q').classList.toggle('active');
+    console.log("SPEED CHANGED");
 });
 
 lastTime = new Date().getTime();
